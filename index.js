87869a7f12767365dc70fbc3f9b52276f15c79ce
Leila Abdi <leila.abdi@student.moringaschool.com>
2023-12-12 21:02:13 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun()); 
+  console.log(saturdayFun('go hiking'));
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  console.log(mondayWork()); 
+  console.log(mondayWork('work from home')); 
+function wrapAdjective(flair = '*') {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  const highlightWithStars = wrapAdjective('*');
+  console.log(highlightWithStars('amazing')); 
+  const highlightWithBars = wrapAdjective('||');
+  console.log(highlightWithBars('awesome'));   
